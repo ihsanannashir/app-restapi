1437c19989fb67d69291f7b05e62790b59156021
greatspy <ihsanannashir@gmail.com>
2021-06-05 23:10:50 +0700

    successfully shows data from database

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var connection = require('./connection');
 
 exports.index = function(req,res) {
-    response.ok("REST API Success yay!");
+    response.ok("REST API Success yay!" , res);
 };
+
+//Shows data mahasiswa
+exports.showsDataMahasiswa = function(req,res) {
+    connection.query('SELECT * FROM mahasiswa',  function(error, rows, failed){
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+}
