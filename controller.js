a11f0f9f0c9e8f25309178d1344aa7c464cb6263
greatspy <ihsanannashir@gmail.com>
2021-06-06 21:07:37 +0700

    successfully update data

--- controller.js
+++ controller.js
@@ -51,3 +51,21 @@ exports.addMahasiswa = function (req, res) {
         }
     );
 }
+
+//[PUT] change/update data by id
+exports.updateMahasiswa = function (req, res) {
+    var id = req.body.id_mahasiswa;
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var prodi = req.body.prodi;
+
+    connection.query('UPDATE mahasiswa SET nim=?, nama=?, prodi=? WHERE id_mahasiswa=?',[nim, nama, prodi, id],
+        function(error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Data has been updated!", res);
+            }
+        }
+    );
+}
--- routes.js
+++ routes.js
@@ -14,4 +14,7 @@ module.exports = function(app) {
 
     app.route('/addMahasiswa')
         .post(jsonku.addMahasiswa);
+
+    app.route('/updateMahasiswa')
+        .put(jsonku.updateMahasiswa);
 }
